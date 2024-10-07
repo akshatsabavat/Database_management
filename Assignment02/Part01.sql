30523496830dc5a17b95443b4db7c3e6e39aaa98
akshatsabavat <akshat.sabavat@gmail.com>
2024-10-08 05:03:46 +0530

    Fixed: 5th question

--- Assignment02/Part01.sql
+++ Assignment02/Part01.sql
@@ -5,4 +5,4 @@ select departments.department_name, AVG(employees.salary) as emp_sal_avg from de
 select employee_name, COUNT(*) as total_projects FROM employees join employee_project on employees.employee_id = employee_project.employee_id group by employee_name having total_projects > 1;
 select employee_name, SUM(employee_project.hours_worked) as total_project_hours FROM employees join employee_project on employees.employee_id = employee_project.employee_id group by employee_name;
 select employee_name, position, salary FROM employees where salary = (select max(salary) from employees);
-select employees.employee_name, projects.project_name from employees left join employee_project on employees.employee_id = employee_project.employee_id left join projects on employee_project.project_id = projects.project_id order by employees.employee_name, projects.project_id;
+select distinct employees.employee_name, projects.project_name from employees left join employee_project on employees.employee_id = employee_project.employee_id left join projects on employee_project.project_id = projects.project_id;
